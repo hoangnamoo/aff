0097fd00c63e599b28b010d3821d1c89a4768a38
Viet Nguyen <nguyenviet.060695@gmail.com>
2023-08-12 20:20:14 +0700

    230812 iu Product detail

--- client/src/pages/User/Order/OrderDetail.js
+++ client/src/pages/User/Order/OrderDetail.js
@@ -49,7 +49,7 @@ function OrderDetail({ setIsOpen }) {
                 <div className="text-sm border-t p-1 w-full flex-1 overflow-auto">
                     <div>Danh sách sản phẩm</div>
                     <div className="flex-1 flex flex-col gap-1">
-                        <div className="border p-1 flex gap-2 justify-between w-full overflow-auto">
+                        <div className="border-b py-2 flex gap-2 justify-between w-full overflow-auto">
                             <div className="h-16 aspect-square">
                                 <img
                                     src="https://images.unsplash.com/photo-1434389677669-e08b4cac3105?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1305&q=80"
@@ -58,14 +58,14 @@ function OrderDetail({ setIsOpen }) {
                                 />
                             </div>
                             <div className="flex flex-col flex-1 gap-1 items-center">
-                                <div className="flex gap-1 justify-between w-full">
-                                    <span className="overflow-hidden text-ellipsis line-clamp-2">
+                                <div className="flex gap-2 justify-between w-full">
+                                    <span className="overflow-hidden text-ellipsis line-clamp-1">
                                         Quần thể thao nam Quần thể thao nam Quần
                                         Quần thể thao nam Quần thể thao nam Quần
                                     </span>
                                     <span>99.000đ</span>
                                 </div>
-                                <div className="flex gap-1 justify-between w-full">
+                                <div className="flex gap-2 justify-between w-full">
                                     <span>Coolmate VN</span>
                                     <span>số lượng: 1</span>
                                 </div>
